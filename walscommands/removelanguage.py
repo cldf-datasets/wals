ce9cf422af5bef80c53785349f5da9a7c39ef8fe
xrotwang <xrotwang@googlemail.com>
2020-01-28 12:27:50 +0100

    closes #66

--- walscommands/removelanguage.py
+++ walscommands/removelanguage.py
@@ -6,6 +6,7 @@ from cldfbench_wals import Dataset
 
 def register(parser):
     parser.add_argument('language_id')
+    parser.add_argument('--replacement_id')
 
 
 def run(args):
@@ -17,3 +18,18 @@ def run(args):
 
     for t in ['language', 'walslanguage']:
         ds.rewrite(t + '.csv', lambda r: r if r['pk'] != lpk else None)
+
+    if args.replacement_id:
+        rpk = ds.pk_from_id('language.csv', args.replacement_id)
+    else:
+        rpk = None
+
+    def repl(r):
+        if r['language_pk'] == lpk:
+            if rpk:
+                r['language_pk'] = rpk
+            else:
+                args.log.warning('removing sentence {0}'.format(r))
+                return
+        return r
+    ds.rewrite('sentence.csv', repl)
