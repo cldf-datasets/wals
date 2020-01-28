b0f90f663c27475da3903287f374eb9db7918741
xrotwang <xrotwang@googlemail.com>
2020-01-28 13:16:49 +0100

    closes #70

--- walscommands/removelanguage.py
+++ walscommands/removelanguage.py
@@ -7,6 +7,7 @@ from cldfbench_wals import Dataset
 def register(parser):
     parser.add_argument('language_id')
     parser.add_argument('--replacement_id')
+    parser.add_argument('--full', default=False, action='store_true')
 
 
 def run(args):
@@ -18,6 +19,17 @@ def run(args):
 
     for t in ['language', 'walslanguage']:
         ds.rewrite(t + '.csv', lambda r: r if r['pk'] != lpk else None)
+
+    if args.full:
+        # remove valuesets as well
+        vspks = set(
+            row['pk'] for row in ds.iter_rows('valueset.csv', lambda r: r['language_pk'] == lpk))
+        ds.rewrite('valueset.csv', lambda r: r if r['pk'] not in vspks else None)
+        ds.rewrite('valuesetreference.csv', lambda r: r if r['valueset_pk'] not in vspks else None)
+        vpks = set(
+            row['pk'] for row in ds.iter_rows('value.csv', lambda r: r['valueset_pk'] in vspks))
+        ds.rewrite('value.csv', lambda r: r if r['valueset_pk'] not in vspks else None)
+        ds.rewrite('valuesentence.csv', lambda r: r if r['value_pk'] not in vpks else None)
 
     if args.replacement_id:
         rpk = ds.pk_from_id('language.csv', args.replacement_id)
