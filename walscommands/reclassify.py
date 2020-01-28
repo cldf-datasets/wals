97b5d885d7153e5593c6b0f306f60cf9a5ce7fd7
xrotwang <xrotwang@googlemail.com>
2020-01-28 15:02:30 +0100

    closes #29

--- walscommands/reclassify.py
+++ walscommands/reclassify.py
@@ -18,6 +18,20 @@ def run(args):
     lpk = ds.pk_from_id('language.csv', args.language_id)
     wlang = ds.get_row('walslanguage.csv', lambda r: r['pk'] == lpk)
 
+    if not args.family:
+        # determine the family from the old genus
+        fpk = ds.get_row('genus.csv', lambda r: r['pk'] == wlang['genus_pk'])['family_pk']
+    else:
+        fpk = None
+        for row in ds.iter_rows('family.csv', lambda r: r['name'] == args.family):
+            fpk = row['pk']
+            break
+        if not fpk:
+            # Create a new family
+            # pk,jsondata,id,name,description,markup_description
+            fpk = ds.maxpk('family.csv') + 1
+            ds.add_rows('family.csv', [fpk, '', slug(args.family), args.family, '', ''])
+
     # Find genus:
     gpk = None
     for row in ds.iter_rows('genus.csv', lambda r: r['name'] == args.genus):
@@ -25,24 +39,17 @@ def run(args):
         break
     if not gpk:
         # Create a new genus
-        if not args.family:
-            # determine the genus from the old family
-            fpk = ds.get_row('genus.csv', lambda r: r['pk'] == wlang['genus_pk'])['family_pk']
-        else:
-            fpk = None
-            for row in ds.iter_rows('family.csv', lambda r: r['name'] == args.family):
-                fpk = row['pk']
-                break
-            if not fpk:
-                # Create a new family
-                # pk,jsondata,id,name,description,markup_description
-                fpk = ds.maxpk('family.csv') + 1
-                ds.add_rows('family.csv', [fpk, '', slug(args.family), args.family, '', ''])
-
         gpk = ds.maxpk('genus.csv') + 1
         # pk,jsondata,id,name,description,markup_description,family_pk,subfamily,icon
         ds.add_rows(
             'genus.csv', [gpk, '', slug(args.genus), args.genus, '', '', fpk, '', 'f222222'])
+
+    def recl(row):
+        if row['pk'] == gpk:
+            row['family_pk'] = fpk
+        return row
+    ds.rewrite('genus.csv', recl)
+
     def recl(row):
         if row['pk'] == lpk:
             row['genus_pk'] = gpk
